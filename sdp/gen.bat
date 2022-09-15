00c09721545bd2332e94ddb6067105574b4e6ac1
Jean-Pierre BROCHET <jeanpierre.brochet@ives.fr>
2022-09-15 09:44:52 +0200

    nettoyage

--- sdp/gen.bat
+++
@@ -1,3 +0,0 @@
-del src\org\murillo\abnf\*.java
-java -cp c:\usr\aparser\aparse-2.2.jar com.parse2.aparse.Parser -destdir src\org\murillo\abnf -package org.murillo.abnf src\org\murillo\abnf\sdp.abnf
-copy Terminal$NumericValue.java src\org\murillo\abnf\

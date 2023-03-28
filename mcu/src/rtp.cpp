bdb8d18154cba2c406cc5bcaf27dea965604af47
Jean-Marc Bollon <86233105+jmbIves@users.noreply.github.com>
2023-03-28 18:16:46 +0200

    Correction du bug DEVBACKLOG-91 avec décalage erroné de 6 au lieu de 8 pour un octet

--- mcu/src/rtp.cpp
+++ mcu/src/rtp.cpp
@@ -1389,7 +1389,7 @@ void RTPRedundantPacket::DecodeRedundantPayload(BYTE *payload,DWORD redsize)
 	WORD i = 0;
 
 	//Check if it is the last
-	bool last = !(payload[i]>>7);
+	bool last = !(payload[i] >> 7);
 
 	//Read redundant headers
 	while(!last)
@@ -1423,27 +1423,27 @@ void RTPRedundantPacket::DecodeRedundantPayload(BYTE *payload,DWORD redsize)
 		BYTE type = payload[i++] & 0x7F;
 		//Get offset
 		WORD offset = payload[i++];
-		offset = offset <<6 | payload[i]>>2;
+		offset = offset << 6 | payload[i] >> 2;
 		//Get size
 		WORD size = payload[i++] & 0x03;
-		size = size <<6 | payload[i++];
+		size = size << 8 | payload[i++];
 		//Append new red header
-		headers.push_back(RedHeader(type,offset,skip,size));
+		headers.push_back(RedHeader(type, offset, skip, size));
 		//Skip the redundant payload
 		skip += size;
 		//Check if it is the last
-		last = !(payload[i]>>7);
+		last = !(payload[i] >> 7);
 	}
 	//Get primaty type
 	primaryType = payload[i] & 0x7F;
 	//Skip it
 	i++;
 	//Get redundant payload
-	redundantData = payload+i;
+	redundantData = payload + i;
 	//Get prymary payload
-	primaryData = redundantData+skip;
+	primaryData = redundantData + skip;
 	//Get size of primary payload
-	primarySize = redsize-i-skip;
+	primarySize = redsize - i - skip;
 }
 
 RTPTimedPacket* RTPRedundantPacket::CreatePrimaryPacket()
